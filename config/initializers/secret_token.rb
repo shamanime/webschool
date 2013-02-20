7075c506c61dc1bbdb26a60ac4b97724d9c56b56
Jefferson Q. Venerando <jefferson@shamani.me>
2013-02-20 16:02:47 -0300

    Primeiro commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Webschool::Application.config.secret_token = '735aeee5cd8e2e14cf3f9d9ebf39ea2de7e0ca3209b8929a513d98d569b9e9ba4dc1bf456569db74c624e32a35f37c32ddbcf11c5c5dd7965535a6a50b2c5e86'
